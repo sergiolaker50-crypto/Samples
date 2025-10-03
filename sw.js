5c4572c314a896f2ac16ebcc5653794ba65fed51
sergiolaker50-crypto <sergiolaker50@gmail.com>
2025-10-03 13:05:14 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-const CACHE_NAME = 'sample-logger-cache-v3';
+const CACHE_NAME = 'journal-app-cache-v2'; // Изменил версию, чтобы кэш обновился
+const REPO_NAME = '/Samples/';
+
 const URLS_TO_CACHE = [
-  '/',
-  '/index.html'
+  REPO_NAME,
+  `${REPO_NAME}index.html`,
+  `${REPO_NAME}manifest.json`,
+  `${REPO_NAME}icon.png`,
+  'https://cdn.sheetjs.com/xlsx-0.20.1/package/dist/xlsx.full.min.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap',
+  'https://images.unsplash.com/photo-1756206872838-29593d5e53a7?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэш открыт');
+        return cache.addAll(URLS_TO_CACHE);
+      })
   );
 });
 
@@ -17,20 +25,22 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Старый кэш удален');
+            return caches.delete(cache);
           }
         })
       );
-    }).then(() => self.clients.claim())
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
